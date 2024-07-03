8f8e85f8079f7d4e2cb9384ce8a35ab38a2941d6
yohswha <yohswhaenmanuel@gmail.com>
2024-07-02 20:44:27 -0400

    updates to model

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -7,11 +7,13 @@ namespace SistemaDeGestionDeCondominio.Models
 	{
         [Key]
         public int id { get; set; }
-
+        [required]
         public string? Nombre { get; set; }
+        [required]
         public string? ApellidoPaterno { get; set; }
         public string? ApellidoMaterno { get; set; }
         public DateTime FechaNacimiento { get; set; }
+        [required]
         public string? Cedula { get; set; }
         public string? Telefono { get; set; }
         public string? Email { get; set; }
--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -7,10 +7,14 @@ namespace SistemaDeGestionDeCondominio.Models
 	{
         [Key]
         public int id { get; set; }
+        [required]
         public string? Nombre { get; set; }
+        [required]
         public string? ApellidoPaterno { get; set; }
         public string? ApellidoMaterno { get; set; }
+        [required]
         public DateTime FechaNacimiento { get; set; }
+        [required]
         public string? Cedula { get; set; }
         public string? Telefono { get; set; }
         public string? Email { get; set; }
